01a5ac8afd9423e11c8c39c8d8669caf4c8abe94
Rubens Montanha <gameplaysoriginal@gmail.com>
2023-04-26 09:44:33 -0300

    first commit

---
+++ Assets/Scripts/Webcam.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Webcam : MonoBehaviour
+{
+
+    private WebCamTexture webcam;
+    
+    public RawImage image;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (webcam == null)
+            webcam = new WebCamTexture(WebCamTexture.devices[0].name, Screen.width, Screen.height, 60);
+
+        image.texture = webcam;
+        image.material.mainTexture = webcam;
+        
+        webcam.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/WebcamCube.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Android;
+
+public class WebcamCube : MonoBehaviour
+{
+
+    private WebCamTexture webcam;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        #if PLATFORM_ANDROID
+                if (!Permission.HasUserAuthorizedPermission(Permission.Camera))
+                {
+                    Permission.RequestUserPermission(Permission.Camera);
+                }
+        #endif
+        
+        if (webcam == null)
+            // webcam = new WebCamTexture();
+            webcam = new WebCamTexture(WebCamTexture.devices[0].name, 1980, 1080, 60);
+
+        GetComponent<Renderer>().material.mainTexture = webcam;
+        webcam.Play();
+        
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
